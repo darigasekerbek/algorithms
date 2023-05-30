13afd9fb84b16833a541920b8ac3ae6536d52cf7
Abylai <221714@astanait.edu.kz>
2023-05-30 09:43:00 +0200

    reSize method (assignment 4)

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,3 +1,5 @@
+import javax.management.openmbean.KeyAlreadyExistsException;
+
 public class MyHashTable<K, V> { //Hash table is created with two parameters(K and V).
     private class HashNode<K, V>{ //K represents the key type. V represents the value type
         private K key;
@@ -14,6 +16,7 @@ public class MyHashTable<K, V> { //Hash table is created with two parameters(K a
     private HashNode<K, V>[] chainArray;
     private int M = 11;
     private int size;
+    private static final double L_Factor =0.75;
     public MyHashTable(){
         chainArray = new HashNode[M]; //default size M
     }
@@ -24,7 +27,18 @@ public class MyHashTable<K, V> { //Hash table is created with two parameters(K a
     private int hash(K key){
         return Math.abs(key.hashCode()) % M; // this method computes hash code for specified key
     }
-    public void put(K key, V value){ //this method adds key-value pair to the hash table
+    public void put(K key, V value) throws IllegalArgumentException, KeyAlreadyExistsException { //this method adds key-value pair to the hash table
+        if (key == null || value == null) { // check key and value if they are null or not
+            throw new IllegalArgumentException("Can not be null");
+        }
+        //check if load factor of hash table exceeds a certain threshold, if yes increase the capacity of the hash table
+        if (M * 4 <size ){
+            increaseBucket();
+        }
+        if((double) size / M > L_Factor){
+            reSize(M * 2);
+        }
+
         int index = hash(key); //calc index for given key
         HashNode<K, V> node = new HashNode<>(key, value); //create a new HashNode with given key and value
         if (chainArray[index] == null){
@@ -48,6 +62,26 @@ public class MyHashTable<K, V> { //Hash table is created with two parameters(K a
         }
         size++;
             }
+
+    private void increaseBucket() throws KeyAlreadyExistsException{
+        //this method increase capacity of hash table by doubling number of buckets, and then rehash all
+        //exist entries and redistributing them in new bucket
+        int prevM = M;
+        M = M * 2;
+        size = 0;
+        HashNode<K, V>[] prevChainArray = chainArray;
+        chainArray = new HashNode[M];
+        for (int i=0; i<prevM; i++){
+            HashNode<K, V> node = prevChainArray[i];
+            while (node != null){
+                HashNode<K,V> next = node.next;
+                node.next = null;
+                put(node.key, node.value);
+                node = next;
+            }
+        }
+    }
+
     public V get(K key){ //get the value stored in hash table associated with provided key
         int index = hash(key);
         HashNode<K, V> curr = chainArray[index];
@@ -116,9 +150,23 @@ public class MyHashTable<K, V> { //Hash table is created with two parameters(K a
         for (int i=0; i<M; i++){
             System.out.println("Bucket" + "\t" + i + "\t" + bucSize[i]);
         }
-    }
 
     }
+    private void reSize(int newCapacity){ // defence 4 assignment
+        //// Rehash all existing elements and put them into new buckets
+        HashNode<K,V>[] newChainArray = new HashNode[newCapacity];
+        for (int i=0; i<M; i++){
+            HashNode<K,V> node = chainArray[i];
+            while(node != null){
+                HashNode<K,V> next = node.next;
+                int newInd = hash(node.key);
+                node.next = newChainArray[newInd];
+                newChainArray[newInd]= node;
+                node = next;
+            }
+        }
+    }
+}
 
 
 
